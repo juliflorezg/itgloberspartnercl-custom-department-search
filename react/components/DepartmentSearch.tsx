e68077e33451d2fbc48279170cfc360eb2e113fa
juliflorezg <forezjuli08@hotmail.com>
2022-11-23 15:45:33 -0500

    feat: custom-department-search, add  SearchBar component

--- react/components/DepartmentSearch.tsx
+++ react/components/DepartmentSearch.tsx
@@ -2,15 +2,17 @@ import React, {useEffect, useState} from 'react'
 import { useQuery } from 'react-apollo'
 import QUERY_VALUE from '../graphql/getDepartmentGroup.gql'
 
-// import { SearchBar } from 'vtex.store-components'
+import { SearchBar } from 'vtex.store-components'
 import DepartmentGroup from './DepartmentGroup'
 
 const DepartmentSearch = () => {
   const {data, loading, error} = useQuery(QUERY_VALUE)
   const [departments, setDepartments] = useState()
+  const [slug, setSlug] = useState<string | null>('')
 
   console.log({departments})
   console.log("Datos de mi query::::",{data, loading, error});
+  console.log("El slug seleccionado es |>|>|>", slug)
 
   useEffect(()=>{
     setDepartments(data?.categories)
@@ -22,7 +24,12 @@ const DepartmentSearch = () => {
      ? <div>Cargando...</div>
      :
      <>
-      <DepartmentGroup departments={departments}/>
+      <DepartmentGroup departments={departments} handleSetSlug={setSlug}/>
+     <SearchBar
+      customSearchPageUrl={slug}
+      placeHolder = "Busca por departamentos"
+      displayMode = "search-and-clear-button"
+     />
      </>
   )
 }
