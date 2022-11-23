43ee488d4b40c7c1ccf3330f5a624d2812224fbc
juliflorezg <forezjuli08@hotmail.com>
2022-11-23 15:44:59 -0500

    feat: custom-department-search, add  logic to component

--- react/components/DepartmentGroup.tsx
+++ react/components/DepartmentGroup.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 type Props = {
   departments: Category[] | undefined
+  handleSetSlug: React.Dispatch<React.SetStateAction<string | null>>
 }
 
 type Category = {
@@ -10,12 +11,23 @@ type Category = {
   slug: string
 }
 
-const DepartmentGroup:React.FC<Props> = ({departments}) => {
+const DepartmentGroup:React.FC<Props> = ({departments, handleSetSlug}) => {
   console.log('Mi grupo de departamentos es:::---:::', departments);
-return (
 
-  <select defaultValue="value0">
+  const departmentOptions: JSX.Element[] | undefined = departments?.map((department: Category) => (
+    <option key={department.id} value={department.slug}>{department.name}</option>
+  ))
+
+  const onHandleSetSlug = (e: React.FormEvent<HTMLSelectElement>) => {
+    handleSetSlug(`${e.currentTarget.value}/$\{term\}&map=ft`)
+    console.log(e.currentTarget.value)
+    // handleSetSlug(e.target.v)
+  }
+
+return (
+  <select defaultValue="value0" onChange={onHandleSetSlug}>
     <option disabled value="value0">Selecciona una opción</option>
+    {departmentOptions}
   </select>
 
 )}
